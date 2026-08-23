5f8c95715d2180d918495f2f55f1f15541fb6ef4
risCodeSharp <rishavsingh2006@gmail.com>
2026-08-23 12:04:34 +0530

    updating ./bootstrap-vcpkg to not send data back to microsoft for data collection

--- setup-vcpkg.ps1
+++ setup-vcpkg.ps1
@@ -3,5 +3,7 @@ echo "Clonning vcpkg.git"
 git clone https://www.github.com/microsoft/vcpkg.git
 
 Set-Location .\vcpkg
-.\bootstrap-vcpkg.bat
+# The data collected by Microsoft is anonymous.
+# You can opt-out of telemetry by re-running the bootstrap-vcpkg script with -disableMetrics,
+.\bootstrap-vcpkg.bat -disableMetrics
 Set-Location ..\
